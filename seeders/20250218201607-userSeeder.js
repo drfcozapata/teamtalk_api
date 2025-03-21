a4ab167af97b68ae27ac44efb9dd42e40a42f18e
Francisco Zapata <drfcozapata@gmail.com>
2025-03-21 16:40:56 -0400

    Feat: added usersData.json to be used by seeder

--- seeders/20250218201607-userSeeder.js
+++ seeders/20250218201607-userSeeder.js
@@ -1,53 +1,38 @@
 "use strict";
-
 const bcrypt = require("bcryptjs");
 const { User, PersonalProfile } = require("../models");
+const usersData = require("../data/usersData.json");
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const users = [
-      {
-        name: "Francisco Zapata",
-        email: "drfcozapata@gmail.com",
-        password:
-          "$2a$10$uvnCeNMUg/68fWYj74/Ec.py3q2mkc0iqOCliM.0En8Zb7ajwHUsu",
-        role: "SuperAdmin",
-        blocked: false,
-      },
-      {
-        name: "James Bond",
-        email: "bond007@mi5.gov.uk",
-        password:
-          "$2a$10$uvnCeNMUg/68fWYj74/Ec.py3q2mkc0iqOCliM.0En8Zb7ajwHUsu",
-        role: "Administrador",
-        blocked: false,
-      },
-      {
-        name: "John Doe",
-        email: "john@example.com",
-        password:
-          "$2a$10$uvnCeNMUg/68fWYj74/Ec.py3q2mkc0iqOCliM.0En8Zb7ajwHUsu",
-        role: "Empleado",
-        blocked: true,
-      },
-      {
-        name: "Jane Doe",
-        email: "jane@example.com",
-        password:
-          "$2a$10$uvnCeNMUg/68fWYj74/Ec.py3q2mkc0iqOCliM.0En8Zb7ajwHUsu",
-        role: "Empleado",
-        blocked: false,
-      },
-    ];
-
-    for (const userData of users) {
-      const user = await User.create(userData);
+    for (const userData of usersData) {
+      const user = await User.create({
+        name: userData.name,
+        email: userData.email,
+        password: bcrypt.hashSync("password", 10),
+        role: userData.role,
+        blocked: userData.blocked,
+        profile_photo_path: userData.profile_photo_path,
+      });
 
       await PersonalProfile.create({
         user_id: user.id,
-        pecodigo: String(user.id).padStart(4, "0"),
-        penombre: user.name,
-        peemail: user.email,
+        pecodigo: userData.profile.pecodigo,
+        penombre: userData.profile.penombre,
+        pecedula: userData.profile.pecedula,
+        petelefono: userData.profile.petelefono,
+        peemail: userData.profile.peemail,
+        pesalario: userData.profile.pesalario,
+        pedepartamento: userData.profile.pedepartamento,
+        pepuesto: userData.profile.pepuesto,
+        pejefe: userData.profile.pejefe,
+        pefoto: userData.profile.pefoto,
+        penumasegurado: userData.profile.penumasegurado,
+        pefechaingreso: userData.profile.pefechaingreso,
+        pecontrato: userData.profile.pecontrato,
+        peasociado: userData.profile.peasociado,
+        peestado: userData.profile.peestado,
+        pediasvacaciones: userData.profile.pediasvacaciones,
       });
     }
   },
